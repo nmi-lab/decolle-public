a007170addb06574088db468e1b863f8f826bb15
stanzarella <simone.tanzarella@iit.it>
2021-05-24 11:39:15 +0200

    updated code

--- decolle/lenet_decolle_model_1D_MN.py
+++ decolle/lenet_decolle_model_1D_MN.py
@@ -37,7 +37,6 @@ class LenetDECOLLE1DMN(DECOLLEBaseMN):
         # Check Nhid and Mhid
         PS = np.array(pool_size)
 
-
         Nhid = list(Nhid[Nhid>0])
         num_conv_layers = len(Nhid)
         pool_size = list(PS[0:num_conv_layers])
